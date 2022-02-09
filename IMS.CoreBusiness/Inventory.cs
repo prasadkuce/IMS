2c7bb6d11d236f70585b3b914bc111afa87cde21
Prasad Gopishetty <prasadkuce@gmail.com>
2022-02-09 22:19:38 +0530

    Add project files.

---
+++ IMS.CoreBusiness/Inventory.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        [Required]
+        public string InventoryName { get; set; }
+        [Range(0,int.MaxValue, ErrorMessage = "{0} must be greater than equal to 0")]
+        public int Quantity { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "{0} must be greater than equal to 0")]
+        public double Price { get; set; }
+    }
+}
---
+++ IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.PluginInterfaces
+{
+    public interface IInventoryRepository
+    {
+        Task<IEnumerable<Inventory>> GetInventoriesByName(string name);
+        Task AddInventoryAsync(Inventory inventory);
+    }
+}
